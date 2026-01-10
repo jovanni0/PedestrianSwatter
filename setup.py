f7babf34f4c12e296dd4fc30653c7e5482093636
yourname <you@example.com>
2026-01-10 23:31:15 +0200

    trying to get the package together:

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'version1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Include your world files
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jovanni',
+    maintainer_email='jovanni@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'camera_node = version1.camera:main',
+        ],
+    },
+)
