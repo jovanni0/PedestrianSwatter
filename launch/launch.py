aa5a08ccaec747f042208610d3c49f8700d16e78
yourname <yourname>
2026-02-03 15:04:43 +0200

    moved models in repo

--- launch/launch.py
+++ launch/launch.py
@@ -1,7 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
@@ -11,7 +11,17 @@ def generate_launch_description():
     pkg_tb3_gazebo = get_package_share_directory('turtlebot3_gazebo')
 
     launch_dir = os.path.dirname(os.path.realpath(__file__))
+    pkg_models_path = os.path.join(launch_dir, '..', 'models')
     world = os.path.join(launch_dir, '..', 'worlds', 'track-v2.world')
+
+    set_gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=[os.environ.get('GAZEBO_MODEL_PATH', ''), ':', pkg_models_path]
+    )
+    set_turtlebot_model = SetEnvironmentVariable(
+        name='TURTLEBOT3_MODEL',
+        value="waffle_pi"
+    )
 
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
@@ -32,6 +42,8 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
+    ld.add_action(set_gazebo_model_path)
+    ld.add_action(set_turtlebot_model)
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
